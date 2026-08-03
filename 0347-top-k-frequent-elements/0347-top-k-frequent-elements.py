c698f42dd552cac07136c738e8abe723db0881aa
Shovan05 <153458881+Shovan05@users.noreply.github.com>
2026-08-03 06:29:26 +0530

    Time: 15 ms (33.2%), Space: 20 MB (10.24%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        seen={}
+        ans=[]
+        for i in nums:
+            if i in seen:
+                seen[i]+=1
+            else:
+                seen[i]=1
+        bucket=[[] for _ in range(len(nums)+1)]
+        for i in seen:
+            bucket[seen[i]].append(i)
+        
+
+        for index in range(len(bucket)-1, 0, -1):
+            for num in bucket[index]:       
+                if k!=0:
+                    ans.append(num)
+                    k-=1
+                else:
+                    break
+        return ans     
