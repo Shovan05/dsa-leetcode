3dcf9ca82b0ed71cb1a0d712b7b4560c7724ae05
Shovan05 <153458881+Shovan05@users.noreply.github.com>
2026-08-08 04:55:36 +0530

    Time: 35 ms (82.42%), Space: 18.9 MB (97.35%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -4,28 +4,13 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        '''left=0
-        right=1
-        ans=[0]
-        while True:
-            if right == len(prices):
-                break
-            if prices[right]<prices[left] and right!=(len(prices)-1) :
-                left=right
-                right+=1
-            ans.append(prices[right]-prices[left])
-            right+=1
-        return max(ans)'''
-
-        ans=[]
-        left=prices[0]
-        for i in range (len(prices)):
-            if i==0:
-                ans.append(0)
-            else :
-                ans.append(min(left,prices[i]))
-                left=min(left,prices[i])
-        for i in range (1,len(ans)):
-            ans[i]=prices[i]-ans[i]
-        return (max(ans))
+        min=prices[0]
+        max=0
+        for price in prices:
+            if price<min:
+                min=price
+            profit=price-min
+            if profit>max:
+                max=profit
+        return max
             
